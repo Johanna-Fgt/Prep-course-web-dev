e4555552a68df76e45c539554241a07823460c93
Johanna-Fgt <100151239+Johanna-Fgt@users.noreply.github.com>
2022-06-24 12:29:18 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Prep-course-web-dev
+# prep-course-web-dev-starter
+
 Created with CodeSandbox
+
+Cours préparatoire pour la formation de développeur web et mobile par la Wild Code School.
---
+++ script.js
@@ -0,0 +1,10 @@
+// Contact Form
+// const message =
+//   "Thank you for submitting your message to the Argo ship. Jason and his crew will respond shortly.";
+
+// const contact = document.getElementById("contact");
+
+// contact.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   alert(message);
+// });
